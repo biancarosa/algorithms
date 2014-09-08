33a8af6d13b80bd917de4bfe38b986ce8319c706
Bianca Rosa <me@biancarosa.com.br>
2014-09-08 11:45:34 -0300

    Update sieve-of-eratosthenes.cpp

--- sieve-of-eratosthenes.cpp
+++ sieve-of-eratosthenes.cpp
@@ -6,7 +6,7 @@ bool primes[MAX];
 int prime[MAX];
 
 /** Generates a boolean array where you can check if first 10.000.000 numbers are primes
- * using if (primes[i])  **/
+    using if (primes[i])  **/
 int fillPrimes(){
     for (unsigned long long i = 0; i < MAX; ++i) {
         primes[i] = true;
@@ -23,8 +23,8 @@ int fillPrimes(){
 }
 
 /** It does the same as the first method but 
- *  also generates an array with the first 10.000.000 prime numbers.
-	You can get the n-th prime with prime[i] **/
+ *  also generates an array with the first prime numbers. 
+    You can get the n-th prime with prime[i] **/
 int fillPrime() {
 	int c = 1;
 	for (unsigned long long i = 0; i < MAX; ++i) {
@@ -46,9 +46,9 @@ int main() {
 	fillPrimes(); fillPrime();
 	cout << "2 is not prime " << primes[2] << endl;
 	cout << "49 is not prime " << primes[49] << endl; 
-	cout << "63 is  prime " << primes[63] << endl; 
+	cout << "17 is  prime " << primes[17] << endl; 
 	cout << "2nd prime " << prime[2] << endl;
 	cout << "49th prime " << prime[49] << endl; 
-	cout << "63th prime " << prime[63] << endl; 
+	cout << "17th prime " << prime[63] << endl; 
 	return 0;
 }
