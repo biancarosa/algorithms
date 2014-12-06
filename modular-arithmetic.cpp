f098f7b5f833f3a3b2d7b8ba2aad33cb75a454ef
Bianca Rosa <me@biancarosa.com.br>
2014-12-05 23:09:03 -0200

    Update modular-arithmetic.cpp

--- modular-arithmetic.cpp
+++ modular-arithmetic.cpp
@@ -1,7 +1,37 @@
+
 #include <iostream>
+#include <string>
+#include <sstream>
 #define ULL unsigned long long
 using namespace std;
+using namespace std;
 
+/** useful for big numbers, read it as a string */
+bool isDivisible_Str(string s, ULL n){
+	ULL lastRemainder = 0;
+	int sz = s.length();
+	for (int i = 0; i < sz; ++i) {
+		lastRemainder = (lastRemainder * 10 + (s[i] - '0')) % n;
+	}
+	return (lastRemainder==0?true:false);
+}
+
+/** it also works for smaller numbers */
+bool isDivisible(ULL p, ULL q){
+	ULL lastRemainder = 0;
+	ULL r;
+	int digits[1000];
+	int count = 0;
+	while (p > 0) {
+		digits[count++] = p % 10;
+		p /= 10;
+	}
+	for (int i = count-1; i >= 0; --i) {
+		lastRemainder = (lastRemainder * 10 + digits[i]) % q;
+	}
+	return (lastRemainder==0?true:false);
+}
+    
 ULL modular_pow(ULL base, ULL exponent, ULL modulus) {
 	ULL result = 1;
 	base = base % modulus;
@@ -21,5 +51,9 @@ int main() {
 	cout << modular_pow(2, 3, 5) << endl;
 	cout << modular_pow(2, 4, 5) << endl;
 	cout << modular_pow(2, 5, 5) << endl;
+	cout << isDivisible_Str("2000", 2) << endl;
+	cout << isDivisible(2000,2) << endl;
+	cout << isDivisible_Str("2001", 2) << endl;
+	cout << isDivisible(2001,2) << endl;
 	return 0;
 }
